f12d73ab613076ed2077566aff9e3ec84d2292e6
Dario Bosco <dario_bosco@protonmail.com>
2021-07-19 13:35:38 +0200

    Esercizio Completato

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
-//Scrivi un programma che stampi i numeri da 1 a 100,
-//ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
-//Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+//Scrivi un programma che stampi i numeri da 1 a 100
+
+var iterations = 100;
+
+for (let i = 0; i < iterations; i++) {
+	var element = i + 1;
+	//Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+	if (element % 3 == 0 && element % 5 == 0) {
+		element = 'FizzBuzz';
+		//ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+	} else if (element % 3 == 0) {
+		element = 'Fizz';
+	} else if (element % 5 == 0) {
+		element = 'Buzz';
+	}
+
+	console.log(element);
+}
